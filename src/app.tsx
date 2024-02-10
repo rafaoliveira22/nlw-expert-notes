10baa58e7f2a7b25e8624e9e8f1483f1983559ef
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2024-02-10 11:26:57 -0300

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+export  function App() {
+  return <h1>Hello World</h1>
+}
+
+// export != export default
+// export -> quando o componente for importado
+// é necessário usar { App } e o nome do componente
+// ex: import { App } from './app'
+// export default -> permite o componente ser nomeado,
+// na importação, com um nome difente do componente
+// ex: import Batata from './app'
+
